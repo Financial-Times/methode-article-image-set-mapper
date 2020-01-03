e805568cdd801b3a4f1f120d504a73cb3cdb9d48
marina <marina.chompalova@ft.com>
2020-01-03 17:42:02 +0200

    Fix linter errors

--- routing.go
+++ routing.go
@@ -4,9 +4,9 @@ import (
 	"net/http"
 
 	status "github.com/Financial-Times/service-status-go/httphandlers"
-	"github.com/sirupsen/logrus"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
 )
 
 type routing struct {
